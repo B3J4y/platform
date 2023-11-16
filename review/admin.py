47f687e8f14fc3f6129c7912ad99f6571c3c63ef
Jan Bernoth <jan.bernoth@uni-potsdam.de>
2023-11-16 11:03:11 +0100

    added data model for review actions
    * new models:
      * Person
      * Institution
      * Notification
      * Role

--- review/admin.py
+++ review/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from review.models import Role, Position, Notification, Person, Institution
+
 # Register your models here.
+admin.site.register(Institution)
+admin.site.register(Notification)
+admin.site.register(Person)
+admin.site.register(Role)
+admin.site.register(Position)
--- review/models.py
+++ review/models.py
@@ -1,3 +1,50 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Role(models.Model):
+    """
+    Role in the logic of the quality system, in which editors,
+    reviewers and submitters have different views
+    """
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Position(models.Model):
+    """
+    In academics are different levels of seniority, like junior researcher,
+    senior researcher, prof and so on
+    """
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Institution(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Person(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    roles = models.ManyToManyField(Role)
+    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Notification(models.Model):
+    message = models.CharField(max_length=500)
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    seen = models.BooleanField()
+
